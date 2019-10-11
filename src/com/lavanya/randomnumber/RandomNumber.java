9a7aa169f069b4015549f13f6db14c06b042a926
Lavanya Mouhamadouvahap <lramapathiran@gmail.com>
2019-10-11 22:14:16 +0200

    Création de la méthode getRandom() et main pour générer une combinaison de 4 chiffres aléatoires et l'afficher

--- src/com/lavanya/randomnumber/RandomNumber.java
+++ src/com/lavanya/randomnumber/RandomNumber.java
@@ -1,9 +1,33 @@
 package com.lavanya.randomnumber;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RandomNumber {
+	
+	public static List<Integer> getRandom(){		
+		List<Integer> randomCombi = new ArrayList<>();
+		randomCombi.add(0);
+		randomCombi.add(0);
+		randomCombi.add(0);
+		randomCombi.add(0); 
+		
+		Random random = new Random();
+
+		for (int i = 0; i < randomCombi.size(); i++){
+			int j = random.nextInt(10);
+			randomCombi.set(i,j);
+		};
+		
+		return randomCombi;
+	}
+	
+	
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(getRandom());
 
 	}
 
