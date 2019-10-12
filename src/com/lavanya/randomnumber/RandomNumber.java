eeb544f0dfad9d4cf705176fbac95c7d8b9046b0
Lavanya Mouhamadouvahap <lramapathiran@gmail.com>
2019-10-12 23:05:04 +0200

    RandomNumber modified

--- src/com/lavanya/randomnumber/RandomNumber.java
+++ src/com/lavanya/randomnumber/RandomNumber.java
@@ -1,9 +1,29 @@
 package com.lavanya.randomnumber;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RandomNumber {
+	
+	public static List<Integer> getRandom(){		
+		List<Integer> randomCombi = new ArrayList<>();
+		
+		Random random = new Random();
+
+		for (int i = 0; i < 4; i++){
+			int j = random.nextInt(10);
+			randomCombi.add(j);
+		};
+		
+		return randomCombi;
+	}
+	
+	
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(getRandom());
 
 	}
 
