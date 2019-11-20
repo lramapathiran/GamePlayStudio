919c64f77b3bd1a440e0c00097031da23ed41925
Lavanya Mouhamadouvahap <lramapathiran@gmail.com>
2019-11-20 10:06:21 +0100

    Creation de l'interface DefenseurDuel et de la classe UtileDefenseurDuel pour les méthodes communes à Defenseur et Duel et modif de la classe Defenseur avec l'interface créée

--- src/com/lavanya/defenseur/Defenseur.java
+++ src/com/lavanya/defenseur/Defenseur.java
@@ -2,17 +2,17 @@ package com.lavanya.defenseur;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+
 import com.lavanya.utile.Utile;
+import com.lavanya.utile.UtileDefenseurDuel;
 import com.lavanya.common.DefenseurChallengerDuel;
+import com.lavanya.common.DefenseurDuel;
 
 public class Defenseur {
 	
 	static DefenseurChallengerDuel dCD = new Utile();
+	static DefenseurDuel dD = new UtileDefenseurDuel();
 	
 	
 	public static void menuStart() {
@@ -22,80 +22,6 @@ public class Defenseur {
 		System.out.println(
 				"C'est fait? Garder bien en tête votre combinaison à 4 chiffres\nMaintenant c'est à moi de jouer!");
 	}
-
-
-
-//Méthode pour créer une nouvelle combinaison à 4 chiffres par l'ordinateur
-	public static Integer newRandom(int min, int max) {
-
-		Random random = new Random();
-		int j = min + random.nextInt(max - min);
-		return j;
-	}
-
-//	Méthode où Le joueur va comparer la combi proposée à la sienne avec les valeurs + = ou -
-//	Conversion de la réponse du joueur en String List ArrayList
-	public static List<String> playerAnswer() {
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println(
-				"Veuillez indiquer + = ou - pour chaque chaque valeur de la combinaison proposée: ex +,=,=,-(merci d'utiliser le même format de réponse que dans l'exemple)");
-		String clue = sc.nextLine();
-//		sc.close();	
-		List<String> playerClue = Arrays.asList(clue.split(","));
-		return playerClue;
-
-	}
-	
-	public static List<Proposition> rangeArray() throws IOException {
-		
-		List<Proposition> limit = new ArrayList<Proposition>();
-				
-		for (int i = 0; i < dCD.properties("digit"); i++) {
-			limit.add(new Proposition(0,10));
-		}
-		
-		return limit;
-	}
-
-//	L'ordinateur va générer une nouvelle combinaison en fonction des indices +,= ou - 
-//	indiqué par le joueur
-	public static List<Integer> runConditions(List<String> y, List<Integer> x, List<Proposition> range) {
-		
-				
-		List<Integer> randomCombiNewTry = new ArrayList<>();
-		
-		for (int j = 0, k = 0, z = 0; j < y.size(); j++, k++) {
-			
-			if (y.get(j).contentEquals("+")) {
-				range.get(j).setMin(x.get(k) + 1);
-				int min = range.get(j).getMin();
-				int max = range.get(j).getMax();
-				
-				z = newRandom(min, max);
-			}
-
-			if (y.get(j).contentEquals("-")) {
-				range.get(j).setMax(x.get(k));
-				int min = range.get(j).getMin();
-				int max = range.get(j).getMax();
-				
-				z = newRandom(min, max);
-			}
-			
-			
-
-			if (y.get(j).contentEquals("=")) {
-				z = x.get(k);
-			}
-			
-			randomCombiNewTry.add(z);
-		}
-		
-		
-		return randomCombiNewTry;
-
-	}
 	
 	
 // Création de la boucle for pour donner à l'ordi la possibilité d'avoir 3
@@ -103,7 +29,7 @@ public class Defenseur {
 	public static void threeAttempt(List<Integer> x, List<Proposition> range) throws IOException {
 		for (int i=0; i<dCD.properties("digitAttempt"); i++) {
 			
-			List<String> y = playerAnswer();
+			List<String> y = dD.playerAnswer();
 		
 			
 			if (dCD.winAnswer(y) == true){
@@ -115,11 +41,11 @@ public class Defenseur {
 						
 			else {					
 				
-				x = runConditions(y, x, range);
+				x = dD.runConditions(y, x, range);
 				System.out.println("Voila ma nouvelle proposition " + x);
 				
 				if (i == 1) {
-					List<String> yLast = playerAnswer();
+					List<String> yLast = dD.playerAnswer();
 				
 				
 					if (dCD.winAnswer(yLast) == false) {
@@ -143,7 +69,7 @@ public class Defenseur {
 		
 		System.out.println("Voici ma première proposition: " + x);
 		
-		List<Proposition> range = rangeArray();
+		List<Proposition> range = dD.rangeArray();
 		
 		threeAttempt(x, range);
 
---
+++ src/com/lavanya/utile/UtileDefenseurDuel.java
@@ -0,0 +1,90 @@
+package com.lavanya.utile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import com.lavanya.common.DefenseurDuel;
+import com.lavanya.defenseur.Proposition;
+import com.lavanya.utile.Utile;
+import com.lavanya.common.DefenseurChallengerDuel;
+
+public class UtileDefenseurDuel implements DefenseurDuel{
+	
+	static DefenseurChallengerDuel dCD = new Utile();
+	
+	//Méthode pour créer une nouvelle combinaison à 4 chiffres par l'ordinateur
+		public Integer newRandom(int min, int max) {
+
+			Random random = new Random();
+			int j = min + random.nextInt(max - min);
+			return j;
+		}
+		
+//		Méthode où Le joueur va comparer la combi proposée à la sienne avec les valeurs + = ou -
+//		Conversion de la réponse du joueur en String List ArrayList
+		public List<String> playerAnswer() {
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println(
+					"Veuillez indiquer + = ou - pour chaque chaque valeur de la combinaison proposée: ex +,=,=,-(merci d'utiliser le même format de réponse que dans l'exemple)");
+			String clue = sc.nextLine();
+//			sc.close();	
+			List<String> playerClue = Arrays.asList(clue.split(","));
+			return playerClue;
+
+		}
+		
+		public List<Proposition> rangeArray() throws IOException {
+			
+			List<Proposition> limit = new ArrayList<Proposition>();
+					
+			for (int i = 0; i < dCD.properties("digit"); i++) {
+				limit.add(new Proposition(0,10));
+			}
+			
+			return limit;
+		}
+		
+//		L'ordinateur va générer une nouvelle combinaison en fonction des indices +,= ou - 
+//		indiqué par le joueur
+		public List<Integer> runConditions(List<String> y, List<Integer> x, List<Proposition> range) {
+			
+					
+			List<Integer> randomCombiNewTry = new ArrayList<>();
+			
+			for (int j = 0, k = 0, z = 0; j < y.size(); j++, k++) {
+				
+				if (y.get(j).contentEquals("+")) {
+					range.get(j).setMin(x.get(k) + 1);
+					int min = range.get(j).getMin();
+					int max = range.get(j).getMax();
+					
+					z = newRandom(min, max);
+				}
+
+				if (y.get(j).contentEquals("-")) {
+					range.get(j).setMax(x.get(k));
+					int min = range.get(j).getMin();
+					int max = range.get(j).getMax();
+					
+					z = newRandom(min, max);
+				}
+				
+				
+
+				if (y.get(j).contentEquals("=")) {
+					z = x.get(k);
+				}
+				
+				randomCombiNewTry.add(z);
+			}
+			
+			
+			return randomCombiNewTry;
+
+		}
+}
