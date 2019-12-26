3dd2bf5640901477ea40a6abb74ee9cd137da474
Lavanya Mouhamadouvahap <lramapathiran@gmail.com>
2019-12-26 16:12:09 +0100

    Amélioration du contenu texte menu de chaque mode

--- src/com/lavanya/menupage/MenuPage.java
+++ src/com/lavanya/menupage/MenuPage.java
@@ -24,7 +24,6 @@ public class MenuPage {
 		System.out.println("1: Défenseur");
 		System.out.println("2: Challenger");
 		System.out.println("3: Duel");
-		System.out.println("4: Quitter l'application");
 		
 		System.out.println("Quel mode de jeu vous intéresse? Veuillez préciser votre choix en saisissant un chiffre 1 à 4: ");
 	}
@@ -32,7 +31,12 @@ public class MenuPage {
 	public List<Integer> menuStartDefenseur() throws IOException {
 		
 		System.out.println("Mode Défenseur");
-		System.out.println("Bienvenue dans le mode Défenseur!");		
+		System.out.println("Bienvenue dans le mode Défenseur!");	
+		System.out.println("Vous devez me faire découvrir une combinaison de 4 chiffres comprise entre 0000 et 9999!");
+		System.out.println("J'ai 5 tentatives pour y arriver");
+		System.out.println("Pour m'aider; à chaque proposition que je ferai, vous devrez indiquer pour chaque chiffre de cette combinaison si le chiffre est plus grand(+), moins grand(-) ou exact(=) par rapport à votre combinaison à découvrir!");
+		System.out.println("Pour cela vous renseignerez une combinaison de 4 caractères dont les valeurs seront + = ou -!");
+		System.out.println("C'est parti!!");
 		
 		List<Integer> combiToFind = iDefChallDuel.playerCombi();
         
@@ -49,6 +53,8 @@ public class MenuPage {
 		System.out.println("Bienvenue dans le mode Challenger!");
 		System.out.println("Vous devez deviner une combinaison de 4 chiffres comprise entre 0000 et 9999!");
 		System.out.println("Vous avez le droit à 5 tentatives!");
+		System.out.println("Pour vous aider; à chaque proposition que vous ferez, j'indiquerai pour chaque chiffre de cette combinaison si le chiffre est plus grand(+), moins grand(-) ou exact(=) par rapport à la combinaison à découvrir!");
+		System.out.println("Pour cela je renseignerai une combinaison de 4 caractères dont les valeurs seront + = ou -!");
 		System.out.println("Vous êtes prêt? Allez! A vous de jouer!");
 	}
 	
@@ -61,6 +67,8 @@ public class MenuPage {
         
         System.out.println("Votre combinaison secrète à découvrir est " + playerCombiToFind);
 		System.out.println("Nous sommes en mode duel, le premier à découvrir la combinaison secrète de l'autre gagne!");
+		System.out.println("A tour de rôle nous ferons nos propositions et l'adversaire(vous ou moi) devra pour chaque combinaison proposée si le chiffre est plus grand(+), moins grand(-) ou exact(=) par rapport à la combinaison à découvrir!");
+		System.out.println("Pour cela nous renseignerons une combinaison de 4 caractères dont les valeurs seront + = ou -!");
 		System.out.println("Vous êtes prêt? Allez je commence!");
 		
 		return playerCombiToFind;
